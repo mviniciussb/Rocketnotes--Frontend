19b679dccbd44fe5927d8882d7212132394cdf85
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-02-06 19:22:56 -0300

    remove links

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -18,6 +18,12 @@ export function New() {
     setNewLink("");
   }
 
+  function handleRemoveLink(deleted){
+    setLinks(prevState => prevState.filter(link => link !== deleted))
+  }
+
+
+
   return (
     <Container>
       <Header />
@@ -35,7 +41,7 @@ export function New() {
           <Section title="Links úteis">
             {
               links.map((link, index) => (
-                <NoteItem key={String(index)} value={link} onClick={() => {}} />
+                <NoteItem key={String(index)} value={link} onClick={() => handleRemoveLink(link)} />
               ))
             }
             <NoteItem 
