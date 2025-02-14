7c7c7ca72ba72c64f3b58869928421605374f36e
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-02-13 22:03:46 -0300

    Update app.routes.jsx

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
 
 import {New} from "../pages/New"
 import {Home} from "../pages/Home"
@@ -7,12 +7,14 @@ import { Profile } from "../pages/Profile";
 
 
 export function AppRoutes(){
-    return(
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/new" element={<New/>}/>
-            <Route path="profile" element={<Profile/>}/>
-            <Route path="/details/:id" element={<Details/>}/>
-        </Routes>
-    )
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="/details/:id" element={<Details />} />
+
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    );
 }
